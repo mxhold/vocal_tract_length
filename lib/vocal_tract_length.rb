52b2c427106bb737379d87daa9baf10c3e04231b
Max Holder <mxhold@gmail.com>
2014-12-19 18:43:16 -0500

    Fix empty root route

--- lib/vocal_tract_length.rb
+++ lib/vocal_tract_length.rb
@@ -6,5 +6,8 @@ module VocalTractLength
     register Sinatra::Praat
 
     set :public_folder, __dir__ + '/public'
+    get '/' do
+      send_file File.expand_path('index.html', settings.public_folder)
+    end
   end
 end
